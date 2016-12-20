8758bf94ec3c007c22bdf98c955c5ca4d2e2ac8b
singaurav <gaurav.singh.00789@gmail.com>
2016-12-20 22:54:09 +0530

    Add square truce masks.
    With test.

--- test/boardDataTest.cpp
+++ test/boardDataTest.cpp
@@ -15,3 +15,19 @@ TEST_CASE("SQUARE_CONFLICT_MASKS", "[boardData]") {
         }
     }
 }
+
+TEST_CASE("SQUARE_TRUCE_MASKS", "[boardData]") {
+    for (int square = 0; square < BOARD_SQUARE_COUNT; ++square) {
+        BitBoard32 conflict_mask_tri_row[BOARD_TRI_ROW_COUNT];
+
+        for (int tri_row_index = 0; tri_row_index < BOARD_TRI_ROW_COUNT; ++tri_row_index) {
+            for (int square_2 = 0; square_2 < TRI_ROW_SQUARE_COUNT; ++square_2) {
+                if (!conflict(square, tri_row_index * TRI_ROW_SQUARE_COUNT + square_2)) {
+                    REQUIRE((SQUARE_TRUCE_MASKS[square][tri_row_index] & (1UL << square_2)) == 0UL);
+                } else {
+                    REQUIRE((SQUARE_TRUCE_MASKS[square][tri_row_index] & (1UL << square_2)) != 0UL);
+                }
+            }
+        }
+    }
+}

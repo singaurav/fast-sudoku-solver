e5cbe25e8f570ce613031161932ea05f798f1c05
singaurav <gaurav.singh.00789@gmail.com>
2017-01-07 22:17:43 +0530

    Enhance locked candidates.

--- src/inc/pre.hpp
+++ src/inc/pre.hpp
@@ -3,6 +3,7 @@
 
 #include "board.hpp"
 
+void digit_locked_candidates(BitBoard32 tri_rows[]);
 void locked_candidates(Board* board);
 
 #endif // #ifndef PRE_INCLUDED
--- src/pre.cpp
+++ src/pre.cpp
@@ -3,55 +3,84 @@
 #include "bitUtils.hpp"
 #include "io.hpp"
 
-void locked_candidates(Board* board) {
-    BitBoard64 intersecs;
+inline void flip_potential_locked_candidates_intersecs(int index, BitBoard64& intersecs, BitBoard64 processed_intersecs, BitBoard32 tri_rows[]) {
+    for (int nn = 0; nn < INTERSEC_NEIGHBOURS_NEIGHBOURS_COUNT; ++nn) {
+        int nn_index = INTERSEC_NEIGHBOURS_NEIGHBOURS[index][nn];
+        BitBoard32 nn_mask = INTERSEC_MASKS[nn_index];
 
-    for (int d = 0; d < DIGIT_COUNT; ++d) {
-        intersecs = (1ULL << BOARD_INTERSEC_COUNT) - 1;
+        int nn_tri_row_index = inter_mask_tri_row_index(nn_mask);
+        BitBoard32 nn_tri_row_mask = inter_mask_tri_row_mask(nn_mask);
 
-        while(intersecs) {
-            int index = pop_bit_64(&intersecs);
+        if (tri_rows[nn_tri_row_index] & nn_tri_row_mask) {
+            if ((processed_intersecs & (1ULL << nn_index)) == 0ULL) {
+                intersecs |= (1ULL << nn_index);
+            }
+        }
+    }
+}
 
-            int tri_row_index = inter_mask_tri_row_index(INTERSEC_MASKS[index]);
-            BitBoard32 tri_row_mask = inter_mask_tri_row_mask(INTERSEC_MASKS[index]);
+void digit_locked_candidates(BitBoard32 tri_rows[]) {
+    BitBoard64 intersecs, processed_intersecs;
 
-            if (board->digit_state_masks[d].tri_rows[tri_row_index] & tri_row_mask) {
-                BitBoard32 line_neighbour_1 = INTERSEC_MASKS[INTERSEC_LINE_NEIGHBOURS[index][0]];
-                int ln_tri_row_index_1 = inter_mask_tri_row_index(line_neighbour_1);
-                BitBoard32 ln_tri_row_mask_1 = inter_mask_tri_row_mask(line_neighbour_1);
+    intersecs = (1ULL << BOARD_INTERSEC_COUNT) - 1;
+    processed_intersecs = 0ULL;
 
-                BitBoard32 line_neighbour_2 = INTERSEC_MASKS[INTERSEC_LINE_NEIGHBOURS[index][1]];
-                int ln_tri_row_index_2 = inter_mask_tri_row_index(line_neighbour_2);
-                BitBoard32 ln_tri_row_mask_2 = inter_mask_tri_row_mask(line_neighbour_2);
+    while(intersecs) {
+        int index = pop_bit_64(&intersecs);
 
-                BitBoard32 box_neighbour_1 = INTERSEC_MASKS[INTERSEC_BOX_NEIGHBOURS[index][0]];
-                int bn_tri_row_index_1 = inter_mask_tri_row_index(box_neighbour_1);
-                BitBoard32 bn_tri_row_mask_1 = inter_mask_tri_row_mask(box_neighbour_1);
+        int tri_row_index = inter_mask_tri_row_index(INTERSEC_MASKS[index]);
+        BitBoard32 tri_row_mask = inter_mask_tri_row_mask(INTERSEC_MASKS[index]);
 
-                BitBoard32 box_neighbour_2 = INTERSEC_MASKS[INTERSEC_BOX_NEIGHBOURS[index][1]];
-                int bn_tri_row_index_2 = inter_mask_tri_row_index(box_neighbour_2);
-                BitBoard32 bn_tri_row_mask_2 = inter_mask_tri_row_mask(box_neighbour_2);
+        if (tri_rows[tri_row_index] & tri_row_mask) {
+            BitBoard32 line_neighbour_1 = INTERSEC_MASKS[INTERSEC_LINE_NEIGHBOURS[index][0]];
+            int ln_tri_row_index_1 = inter_mask_tri_row_index(line_neighbour_1);
+            BitBoard32 ln_tri_row_mask_1 = inter_mask_tri_row_mask(line_neighbour_1);
 
-                if ((board->digit_state_masks[d].tri_rows[ln_tri_row_index_1] & ln_tri_row_mask_1) == 0UL &&
-                    (board->digit_state_masks[d].tri_rows[ln_tri_row_index_2] & ln_tri_row_mask_2) == 0UL) {
-                    if (board->digit_state_masks[d].tri_rows[bn_tri_row_index_1] & bn_tri_row_mask_1) {
-                        board->digit_state_masks[d].tri_rows[bn_tri_row_index_1] &= ~bn_tri_row_mask_1;
-                    }
+            BitBoard32 line_neighbour_2 = INTERSEC_MASKS[INTERSEC_LINE_NEIGHBOURS[index][1]];
+            int ln_tri_row_index_2 = inter_mask_tri_row_index(line_neighbour_2);
+            BitBoard32 ln_tri_row_mask_2 = inter_mask_tri_row_mask(line_neighbour_2);
 
-                    if (board->digit_state_masks[d].tri_rows[bn_tri_row_index_2] & bn_tri_row_mask_2) {
-                        board->digit_state_masks[d].tri_rows[bn_tri_row_index_2] &= ~bn_tri_row_mask_2;
-                    }
-                } else if ((board->digit_state_masks[d].tri_rows[bn_tri_row_index_1] & bn_tri_row_mask_1) == 0UL &&
-                    (board->digit_state_masks[d].tri_rows[bn_tri_row_index_2] & bn_tri_row_mask_2) == 0UL) {
-                    if (board->digit_state_masks[d].tri_rows[ln_tri_row_index_1] & ln_tri_row_mask_1) {
-                        board->digit_state_masks[d].tri_rows[ln_tri_row_index_1] &= ~ln_tri_row_mask_1;
-                    }
+            BitBoard32 box_neighbour_1 = INTERSEC_MASKS[INTERSEC_BOX_NEIGHBOURS[index][0]];
+            int bn_tri_row_index_1 = inter_mask_tri_row_index(box_neighbour_1);
+            BitBoard32 bn_tri_row_mask_1 = inter_mask_tri_row_mask(box_neighbour_1);
 
-                    if (board->digit_state_masks[d].tri_rows[ln_tri_row_index_2] & ln_tri_row_mask_2) {
-                        board->digit_state_masks[d].tri_rows[ln_tri_row_index_2] &= ~ln_tri_row_mask_2;
-                    }
+            BitBoard32 box_neighbour_2 = INTERSEC_MASKS[INTERSEC_BOX_NEIGHBOURS[index][1]];
+            int bn_tri_row_index_2 = inter_mask_tri_row_index(box_neighbour_2);
+            BitBoard32 bn_tri_row_mask_2 = inter_mask_tri_row_mask(box_neighbour_2);
+
+            if ((tri_rows[ln_tri_row_index_1] & ln_tri_row_mask_1) == 0UL &&
+                (tri_rows[ln_tri_row_index_2] & ln_tri_row_mask_2) == 0UL) {
+                if (tri_rows[bn_tri_row_index_1] & bn_tri_row_mask_1) {
+                    tri_rows[bn_tri_row_index_1] &= ~bn_tri_row_mask_1;
+                    processed_intersecs |= (1ULL << index);
+                    flip_potential_locked_candidates_intersecs(index, intersecs, processed_intersecs, tri_rows);
+                }
+
+                if (tri_rows[bn_tri_row_index_2] & bn_tri_row_mask_2) {
+                    tri_rows[bn_tri_row_index_2] &= ~bn_tri_row_mask_2;
+                    processed_intersecs |= (1ULL << index);
+                    flip_potential_locked_candidates_intersecs(index, intersecs, processed_intersecs, tri_rows);
+                }
+            } else if ((tri_rows[bn_tri_row_index_1] & bn_tri_row_mask_1) == 0UL &&
+                (tri_rows[bn_tri_row_index_2] & bn_tri_row_mask_2) == 0UL) {
+                if (tri_rows[ln_tri_row_index_1] & ln_tri_row_mask_1) {
+                    tri_rows[ln_tri_row_index_1] &= ~ln_tri_row_mask_1;
+                    processed_intersecs |= (1ULL << index);
+                    flip_potential_locked_candidates_intersecs(index, intersecs, processed_intersecs, tri_rows);
+                }
+
+                if (tri_rows[ln_tri_row_index_2] & ln_tri_row_mask_2) {
+                    tri_rows[ln_tri_row_index_2] &= ~ln_tri_row_mask_2;
+                    processed_intersecs |= (1ULL << index);
+                    flip_potential_locked_candidates_intersecs(index, intersecs, processed_intersecs, tri_rows);
                 }
             }
-        }        
+        }
+    }
+}
+
+void locked_candidates(Board* board) {
+    for (int d = 0; d < DIGIT_COUNT; ++d) {
+        digit_locked_candidates(board->digit_state_masks[d].tri_rows);
     }
 }
